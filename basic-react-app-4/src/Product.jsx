c6963d378f6a426ed7ca26a8a5d606b027c4cc4a
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-12-12 09:18:18 +0530

    styling

--- basic-react-app-4/src/Product.jsx
+++ basic-react-app-4/src/Product.jsx
@@ -1,6 +1,8 @@
+import "./Product.css"
+
 function Product(){
     return(
-        <div>
+        <div className="Product">
             <h3>This is a product</h3>
             <h5>Product description</h5>
         </div>
