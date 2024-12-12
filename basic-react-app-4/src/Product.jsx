7106bf829fdb568ba3850b85abe59205c2e613be
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-12-12 09:15:35 +0530

    structure

---
+++ basic-react-app-4/src/Product.jsx
@@ -0,0 +1,10 @@
+function Product(){
+    return(
+        <div>
+            <h3>This is a product</h3>
+            <h5>Product description</h5>
+        </div>
+    )
+}
+
+export default Product;
